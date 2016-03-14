cab418a7e94bef4438c1e232c21593226ac8a21b
xqlcrystal <xql252660@163.com>
2016-03-14 11:51:22 +0800

    模块

---
+++ 15AngularJS 模块/myApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by xieql2 on 2016/3/14.
+ */
+var app=angular.module("myApp",[]);
---
+++ 15AngularJS 模块/myCtrl.js
@@ -0,0 +1,6 @@
+/**
+ * Created by xieql2 on 2016/3/14.
+ */
+app.controler("myCtrl",function($scope){
+    $scope.count=0;
+});
